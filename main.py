2a25bf05177fc52ea2ecc1a00c612a02c24c8613
laomocode <3344907598@qq.com>
2020-01-30 19:41:20 +0800

    恢复原状态

--- main.py
+++ main.py
@@ -10,26 +10,14 @@ data=api.guonei()
 data2=api.data()
 updatetime=strftime("%Y-%m-%d %H:%M:%S",localtime(data2['modifyTime']/1000))
 zhongdata=[]
-deaddata=[]
-zhiyu=[]
 for a in range(len(data)):
     tempdata=[]
-    tempdeaddata=[]
-    tempzhiyu=[]
     tempdata.append(data[a]['provinceShortName'])
     tempdata.append(data[a]['confirmedCount'])
-    tempdeaddata.append(data[a]['provinceShortName'])
-    tempdeaddata.append(data[a]['deadCount'])
-    tempzhiyu.append(data[a]['provinceShortName'])
-    tempzhiyu.append(data[a]['curedCount'])
     zhongdata.append(tempdata)
-    deaddata.append(tempdeaddata)
-    zhiyu.append(tempzhiyu)
-pingjun=(data2['confirmedCount']+data2['deadCount']+data2['curedCount'])/len(data)
-geo.set_global_opts(title_opts=opts.TitleOpts(title="国内疫情地图",subtitle="数据来自丁香园，截止时间："+updatetime),visualmap_opts=opts.VisualMapOpts(max_=pingjun))
+pingjun=data2['confirmedCount']/len(data)
+geo.set_global_opts(title_opts=opts.TitleOpts(title="国内疫情地图",subtitle="数据来自丁香园，截止时间："+updatetime),visualmap_opts=opts.VisualMapOpts(max_=pingjun),legend_opts=opts.LegendOpts(is_show=False))
 geo.add("确诊",zhongdata, maptype="china")
-geo.add("死亡",deaddata,maptype="china")
-geo.add("治愈",zhiyu,maptype="china")
 if exists('dists')==False:
     mkdir('dists')
 chdir('dists')
