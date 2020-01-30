fa4eaaa89aab849f3f571e8c72e7a79c2696cca9
laomocode <3344907598@qq.com>
2020-01-30 18:38:53 +0800

    加入死亡、治愈统计

--- main.py
+++ main.py
@@ -3,26 +3,32 @@ from api import api
 from pyecharts import options as opts
 import os
 import time
-os.environ['TZ'] = 'Asia/Shanghai'
 geo=Map()
 api=api()
 data=api.guonei()
 data2=api.data()
 updatetime=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(data2['modifyTime']/1000))
 zhongdata=[]
-pingjundata=[]
-sum=0
+deaddata=[]
+zhiyu=[]
 for a in range(len(data)):
     tempdata=[]
+    tempdeaddata=[]
+    tempzhiyu=[]
     tempdata.append(data[a]['provinceShortName'])
     tempdata.append(data[a]['confirmedCount'])
-    pingjundata.append(data[a]['confirmedCount'])
+    tempdeaddata.append(data[a]['provinceShortName'])
+    tempdeaddata.append(data[a]['deadCount'])
+    tempzhiyu.append(data[a]['provinceShortName'])
+    tempzhiyu.append(data[a]['curedCount'])
     zhongdata.append(tempdata)
-for a in range(len(pingjundata)):
-    sum=sum+pingjundata[a]
-pingjun=sum/len(pingjundata)
+    deaddata.append(tempdeaddata)
+    zhiyu.append(tempzhiyu)
+pingjun=(data2['confirmedCount']+data2['deadCount']+data2['curedCount'])/len(data)
 geo.set_global_opts(title_opts=opts.TitleOpts(title="国内疫情地图",subtitle="数据来自丁香园，截止时间："+updatetime),visualmap_opts=opts.VisualMapOpts(max_=pingjun))
 geo.add("确诊",zhongdata, maptype="china")
+geo.add("死亡",deaddata,maptype="china")
+geo.add("治愈",zhiyu,maptype="china")
 if os.path.exists('dists')==False:
     os.mkdir('dists')
 os.chdir('dists')
--- mainci.py
+++ mainci.py
@@ -9,20 +9,26 @@ data=api.guonei()
 data2=api.data()
 updatetime=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(data2['modifyTime']/1000+28800))
 zhongdata=[]
-pingjundata=[]
-sum=0
+deaddata=[]
+zhiyu=[]
 for a in range(len(data)):
     tempdata=[]
+    tempdeaddata=[]
+    tempzhiyu=[]
     tempdata.append(data[a]['provinceShortName'])
     tempdata.append(data[a]['confirmedCount'])
-    pingjundata.append(data[a]['confirmedCount'])
+    tempdeaddata.append(data[a]['provinceShortName'])
+    tempdeaddata.append(data[a]['deadCount'])
+    tempzhiyu.append(data[a]['provinceShortName'])
+    tempzhiyu.append(data[a]['curedCount'])
     zhongdata.append(tempdata)
-for a in range(len(pingjundata)):
-    sum=sum+pingjundata[a]
-pingjun=sum/len(pingjundata)
+    deaddata.append(tempdeaddata)
+    zhiyu.append(tempzhiyu)
+pingjun=(data2['confirmedCount']+data2['deadCount']+data2['curedCount'])/len(data)
 geo.set_global_opts(title_opts=opts.TitleOpts(title="国内疫情地图",subtitle="数据来自丁香园，截止时间："+updatetime),visualmap_opts=opts.VisualMapOpts(max_=pingjun))
 geo.add("确诊",zhongdata, maptype="china")
-if os.path.exists('dists')==False:
-    os.mkdir('dists')
+geo.add("死亡",deaddata,maptype="china")
+geo.add("治愈",zhiyu,maptype="china")
+os.mkdir('dists')
 os.chdir('dists')
 geo.render('index.html')
