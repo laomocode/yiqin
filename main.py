b503402c6c4ac704f7b7e0c2bc1e118ae0824b87
laomocode <3344907598@qq.com>
2020-02-13 19:48:05 +0800

    修改确诊从累计确诊改为目前确诊

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ zhongdata=[]
 for a in range(len(data)):
     tempdata=[]
     tempdata.append(data[a]['provinceShortName'])
-    tempdata.append(data[a]['confirmedCount'])
+    tempdata.append(data[a]['currentConfirmedCount'])
     zhongdata.append(tempdata)
-pingjun=data2['confirmedCount']/len(data)
-geo.set_global_opts(title_opts=opts.TitleOpts(title="国内疫情地图",subtitle="共确诊"+str(data2['confirmedCount'])+"例，数据来自丁香园，截止时间："+updatetime),visualmap_opts=opts.VisualMapOpts(max_=pingjun),legend_opts=opts.LegendOpts(is_show=False))
+pingjun=data2['currentConfirmedCount']/len(data)
+geo.set_global_opts(title_opts=opts.TitleOpts(title="国内疫情地图",subtitle="目前共确诊"+str(data2['currentConfirmedCount'])+"例，数据来自丁香园，截止时间："+updatetime),visualmap_opts=opts.VisualMapOpts(max_=pingjun),legend_opts=opts.LegendOpts(is_show=False))
 geo.add("确诊",zhongdata, maptype="china")
 geo.render('index.html')
